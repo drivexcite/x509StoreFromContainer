36d7e110b40a4380cdd89bfd34dbc48da69f1f69
Tona Diaz <tonadiaz@microsoft.com>
2021-10-13 12:54:33 -0600

    Added plausible solution to load the certificates during App initialization

---
+++ Controllers/CertificatesController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace x509storeFromContainer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CertificatesController : ControllerBase
+    {
+        private readonly ILogger<CertificatesController> _logger;
+
+        public CertificatesController(ILogger<CertificatesController> logger)
+        {
+            _logger = logger;
+        }
+
+        private IEnumerable<object> GetCertificateInfo()
+        {
+            var store = new X509Store(StoreName.My, StoreLocation.CurrentUser);
+            store.Open(OpenFlags.ReadOnly);
+
+            foreach(var certificate in store.Certificates)
+            {
+                yield return new 
+                {
+                    certificate.IssuerName,
+                    certificate.FriendlyName,                    
+                    certificate.Subject,
+                    certificate.SubjectName, 
+                    certificate.HasPrivateKey,
+                    certificate.NotAfter,
+                    certificate.NotBefore,           
+                    certificate.Thumbprint
+                };
+            }
+
+            store.Close();
+        }
+
+        [HttpGet]
+        public List<object> Get()
+        {
+            try
+            {
+                return GetCertificateInfo().ToList();
+            }
+            catch (Exception e)
+            {
+                _logger.LogWarning(e, "Something is not quite working");
+            }
+
+            return null;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,19 +1,60 @@
 using System;
-using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Security.Cryptography.X509Certificates;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 namespace x509storeFromContainer
 {
+    public static class HostExtensions
+    {
+        public static IHost AddCertificatesToStore(this IHost host) 
+        {
+            var logger = host.Services.GetService<ILogger<Program>>();
+
+            try
+            {                
+                var certificatesPath = Path.Combine(Environment.CurrentDirectory, "certs");                
+
+                var certificates = (
+                    from file in new DirectoryInfo(certificatesPath).GetFiles("*.pfx") 
+                    let certificateLocation = file.FullName
+                    let base64Representation = File.ReadAllText(certificateLocation)
+                    let binaryRepresentation = Convert.FromBase64String(base64Representation)
+                    select new X509Certificate2(binaryRepresentation)
+                ).ToList();
+
+                logger.LogInformation($"{certificates.Count} certificates found in {certificatesPath}");
+
+                using var store = new X509Store(StoreName.My, StoreLocation.CurrentUser);
+                store.Open(OpenFlags.ReadWrite);
+
+                foreach(var certificate in certificates)
+                {                                                        
+                    store.Add(certificate);
+                    logger.LogInformation($"Added Friendly Name: {certificate.FriendlyName}, Subject Name: {certificate.SubjectName?.Name}, Thumbprint {certificate.Thumbprint}");
+                }
+            }
+            catch(Exception e)
+            {
+                logger.LogWarning(e, "Loading the certificates didn't work");
+            }
+
+            return host;
+        }
+    }
+
     public class Program
     {
         public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
+        {            
+            CreateHostBuilder(args)
+                .Build()
+                .AddCertificatesToStore()
+                .Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
